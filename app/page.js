eba3635085dda725e177c17a9855ebc5ee61b240
Verity <verity.boyd@edu.sait.ca>
2026-02-19 14:00:43 -0700

    fixed bday date

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Page() {
         Happy birthday, Mum!
       </h1>
       <Picture src="/baby.jpeg" alt="Mum holding a photo of her as a baby" />
-      <h2 className="text-xl font-medium text-sky-700">63 glorious years</h2>
+      <h2 className="text-xl font-medium text-sky-700">64 glorious years</h2>
       <button
         onClick={goToSlides}
         className="border-b-sky-500 rounded m-2 px-2 py-1 text-xl font-semibold text-blue-900 bg-sky-100 hover:bg-sky-500"
